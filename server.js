17ce37b286cc62818c8e33db22d0dead1846e1fa
ikarjfak <fl1k3rrr@gmail.com>
2026-05-30 01:17:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-// Middleware
-app.use(cors());
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
